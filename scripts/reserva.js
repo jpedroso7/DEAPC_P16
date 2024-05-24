dd8a2346115f418097f02c36c7786db2b797287f
Daniel Botelho <botelhodaniel1998@gmail.com>
2024-05-24 18:40:56 +0100

    a

--- scripts/reserva.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const bookingForm = document.getElementById('bookingForm');
-    const bookNowButton = document.getElementById('bookNowButton');
-
-    bookingForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Impede o envio do formulário
-        alert('Your booking has been confirmed!');
-    });
-});
